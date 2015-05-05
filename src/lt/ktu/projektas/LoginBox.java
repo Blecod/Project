10f740e6a4656b6fce326d119444bec515e18ed4
Dominykas Barzdys <Dominykas Barzdys@192.168.0.23>
2015-05-05 18:00:57 +0300

    Sukelimas failu

---
+++ src/lt/ktu/projektas/LoginBox.java
@@ -0,0 +1,70 @@
+package lt.ktu.projektas;
+
+
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class LoginBox {
+	private Stage window;
+	private TextField email;
+	private PasswordField password;
+	public LoginBox(){
+		window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Login");
+		window.setWidth(300);
+
+		window.setResizable(false);
+		
+		email = new TextField();
+		password = new PasswordField();
+		
+		Label emailTitle = new Label("E-mail:");
+		Label passwordTitle = new Label("Password:");
+		Button submit = new Button("Login");
+		
+		submit.setOnAction(new onSubmit());
+		
+		email.setMaxWidth(Double.MAX_VALUE);
+		password.setMaxWidth(Double.MAX_VALUE);
+		submit.setMaxWidth(Double.MAX_VALUE);
+		
+		VBox box = new VBox(5);
+		box.setPadding(new Insets(5, 5, 0, 5));
+		box.getChildren().addAll(emailTitle, email, passwordTitle, password, submit);
+		Scene scene = new Scene(box);
+		window.setScene(scene);
+		window.showAndWait();
+	}
+	public String getEmail(){
+		return email.getText();
+	}
+	public String getPassword(){
+		return password.getText();
+	}
+	private class onSubmit implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent arg0) {	
+			if(email.getText().length()==0){
+				AlertBox.show("Fill in  your e-mail!");
+				return;
+			}
+			if(password.getText().length()==0){
+				AlertBox.show("Fill in  your password!");
+				return;
+			}
+			window.close();
+		}
+	}
+}
---
+++ src/lt/ktu/projektas/utils/FieldType.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lt.ktu.projektas.utils;
+import java.awt.Component;
+
+import javafx.scene.control.*;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Domas
+ */
+public enum FieldType {
+    TEXTFIELD,
+    TEXTAREA,
+    INTEGER,
+    DOUBLE,
+    BOOLEAN,
+    COMBOBOX; 
+}
